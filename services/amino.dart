4a594e515f4260f0010f293af73c85aaa47d5050
swornimbarahi <sbara002@plattsburgh.edu>
2020-02-12 23:44:07 -0500

    Created an Amino class

---
+++ services/amino.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Amino {
+  String name;
+  String code;
+  Color color;
+  String structure;
+
+  Amino ({ this.name, this.code, this.color, this.structure});
+}
